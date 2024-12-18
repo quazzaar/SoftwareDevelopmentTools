98bab77db396a97112c30dd4069b629c9c4798b6
dmitrijnadezdin <dn.official@icloud.com>
2024-12-18 21:46:50 +0300

    v 1.6.0

--- circle.py
+++ circle.py
@@ -1,5 +1,5 @@
 import math
-
+import unittest
 
 def area(r):
     '''
@@ -15,6 +15,8 @@ def area(r):
             area(3)
             28.274333882308138
     '''
+    if r < 0:
+        raise ValueError("Радиус должен быть неотрицательным")
     return math.pi * r * r
 
 
@@ -32,4 +34,29 @@ def perimeter(r):
             perimeter(3)
             18.84955592153876
     '''
+    if r < 0:
+        raise ValueError("Радиус должен быть неотрицательным")
     return 2 * math.pi * r
+
+
+print("Ручное тестирование для круга")
+print("Посчитал, что при r = 5 => S ~78.53982")
+print(area(5))
+print("Посчитал, что при r = 5 => S ~31.41593 ")
+print(perimeter(5))
+
+
+class TestCircle(unittest.TestCase):
+    def test_area(self):
+        self.assertAlmostEqual(area(5), 78.53981633974483) # стандартный случай
+        self.assertEqual(area(0), 0) # радиус равен 0
+
+    def test_perimeter(self):
+        self.assertAlmostEqual(perimeter(5), 31.41592653589793)
+        self.assertEqual(perimeter(0), 0)
+
+    def test_negative_values(self):
+        with self.assertRaises(ValueError):
+            area(-5)
+        with self.assertRaises(ValueError):
+            perimeter(-5)
--- square.py
+++ square.py
@@ -1,3 +1,4 @@
+import unittest
 
 def area(a):
     '''
@@ -13,6 +14,8 @@ def area(a):
             area(5)
             25
     '''
+    if a < 0:
+        raise ValueError("Длина стороны должна быть неотрицательной")
     return a * a
 
 
@@ -30,4 +33,29 @@ def perimeter(a):
             perimeter(5)
             20
     '''
+    if a < 0:
+        raise ValueError("Длина стороны должна быть неотрицательной")
     return 4 * a
+
+
+print("Ручное тестирование для квадрата")
+print("Посчитал, что при a = 5 => S = 25")
+print(area(5))
+print("Посчитал, что при a = 5 => P = 20")
+print(perimeter(5))
+
+
+class TestSquare(unittest.TestCase):
+    def test_area(self):
+        self.assertEqual(area(5), 25)  # Стандартный случай
+        self.assertEqual(area(0), 0)  # Сторона равна 0
+
+    def test_perimeter(self):
+        self.assertEqual(perimeter(5), 20)  # Стандартный случай
+        self.assertEqual(perimeter(0), 0)  # Сторона равна 0
+
+    def test_negative_values(self):
+        with self.assertRaises(ValueError):
+            area(-5)
+        with self.assertRaises(ValueError):
+            perimeter(-5)
